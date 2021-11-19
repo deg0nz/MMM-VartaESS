88f0b13fe382ffaee45173e2baa4d0daab79e0cf
Beh <github@beh.wtf>
2021-11-19 01:32:00 +0100

    interval in start func

--- MMM-VartaESS.js
+++ MMM-VartaESS.js
@@ -11,8 +11,7 @@ Module.register("MMM-VartaESS", {
 	defaults: {
         ip: "192.168.200.195",
         port: 502,
-		updateInterval: 3000,
-		retryDelay: 5000
+		updateInterval: 3000
 	},
 
 	requiresVersion: "2.1.0", // Required version of MagicMirror
@@ -24,14 +23,21 @@ Module.register("MMM-VartaESS", {
 		this.loaded = false;
 
         this.sendSocketNotification("MMM-VartaESS_INIT", this.config);
+
+        setInterval(function() {
+            console.log("Interval")
+            if(this.loaded === true) {
+                this.fetchData();
+            }
+        }, this.config.updateInterval);
 	},
 
-	scheduleUpdate: function() {
-        console.log("schedule triggered")
-		setInterval(function() {
-			this.fetchData();
-		}, this.config.updateInterval);
-	},
+	// scheduleUpdate: function() {
+    //     console.log("schedule triggered")
+	// 	setInterval(function() {
+	// 		this.fetchData();
+	// 	}, this.config.updateInterval);
+	// },
 
 	getDom: function() {
         console.log("getDom triggered");
@@ -41,7 +47,7 @@ Module.register("MMM-VartaESS", {
 
         const stateRow = document.createElement("tr");
         const stateDescriptionColumn = document.createElement("td");
-        stateDescriptionColumn.textContent = "Status";
+        stateDescriptionColumn.textContent = "Status:";
         stateRow.appendChild(stateDescriptionColumn);
         const stateValueColumn = document.createElement("td");
         stateValueColumn.textContent = this.currentData.state;
@@ -50,7 +56,7 @@ Module.register("MMM-VartaESS", {
 
         const socRow = document.createElement("tr");
         const socDescriptionColumn = document.createElement("td");
-        socDescriptionColumn.textContent = "Ladung";
+        socDescriptionColumn.textContent = "Ladung:";
         socRow.appendChild(socDescriptionColumn);
         const socValueColumn = document.createElement("td");
         socValueColumn.textContent = `${this.currentData.soc} %`;
@@ -59,7 +65,7 @@ Module.register("MMM-VartaESS", {
 
         const gridPowerRow = document.createElement("tr");
         const gridPowerDescriptionColumn = document.createElement("td");
-        gridPowerDescriptionColumn.textContent = "Stromnetz";
+        gridPowerDescriptionColumn.textContent = "Stromnetz: ";
         gridPowerRow.appendChild(gridPowerDescriptionColumn);
         const gridPowerValueColumn = document.createElement("td");
         const gpValue = this.currentData.gridPower;
@@ -69,7 +75,7 @@ Module.register("MMM-VartaESS", {
 
         const activePowerRow = document.createElement("tr");
         const activePowerDescriptionColumn = document.createElement("td");
-        activePowerDescriptionColumn.textContent = "Batterie";
+        activePowerDescriptionColumn.textContent = "Batterie:";
         activePowerRow.appendChild(activePowerDescriptionColumn);
         const activePowerValueColumn = document.createElement("td");
         const apValue = this.currentData.activePower;
@@ -112,17 +118,14 @@ Module.register("MMM-VartaESS", {
 		// }
 
         if(notification === "MMM-VartaESS_DATA") {
+            if(this.loaded === false) {
+                this.loaded = true;
+            }
+
             this.currentData = payload;
             console.log(`Got data in main module: ${JSON.stringify(payload)}`);
 
             this.updateDom();
-
-            if(this.loaded === false) {
-                this.loaded = true;
-                setInterval(function() {
-                    this.fetchData();
-                }, this.config.updateInterval);
-            }
         }
 	},
 });
